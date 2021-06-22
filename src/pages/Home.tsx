7b1db3289bf22b0c88754271298740ea6d0ce198
Diego Souza <diegosouza@tradecom.com.br>
2021-06-21 22:18:26 -0300

    Criando Context, Manter informações de autenticação do usuário

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,24 @@
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import googleIconImg from '../assets/images/google-icon.svg';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
-
-import '../styles/auth.scss';
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
+import '../styles/auth.scss';
+
+
 
 export function Home() {
   const history = useHistory();
+  const { signInWithGoogle, user } = useAuth()
 
-  function navigateToNewRoom(){
-    history.push('rooms/new')
+  async function handleCreateRoom() {
+
+    if (!user) {
+      await signInWithGoogle()
+    }
+
+    history.push('rooms/new');
   }
 
   return (
@@ -23,16 +31,16 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room" onClick={navigateToNewRoom}>
+          <button className="create-room" onClick={handleCreateRoom}>
             <img src={googleIconImg} alt="Logo da Google" />
             Crie sua sala com o google
           </button>
           <div className="separator">ou entre em uma sala</div>
 
           <form>
-            <input 
-            type="text"
-            placeholder="Digite o codigo da sala" />
+            <input
+              type="text"
+              placeholder="Digite o codigo da sala" />
             <Button type="submit">
               Entrar na sala
             </Button>
